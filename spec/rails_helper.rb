3329628683192f280da5b478b8a4ac565b7deaf5
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-12 14:10:03 -0600

    Add VCR and new fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,4 +75,9 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<keys>') { ENV['WEATHER_API_KEY'] }
+  config.filter_sensitive_data('<keys>') { ENV['MAPQ_API_KEY'] }
+  config.default_cassette_options = { re_record_interval: 30.days }
+  config.allow_http_connections_when_no_cassette = true
 end
