37e1c6dd80278eb15c415fcb432c12295b0dc595
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-12 23:01:11 -0600

    Add image controller and service

---
+++ app/services/background_service.rb
@@ -0,0 +1,19 @@
+class BackgroundService
+  class << self
+    def get_image(location)
+      response = conn.get('/search/photos') do |req|
+        req.params['client_id'] = ENV['SPLASH_API_KEY']
+        req.params['query'] = location
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def conn
+      Faraday.new(url: 'https://api.unsplash.com')
+    end
+
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
