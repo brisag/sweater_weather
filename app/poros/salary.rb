10ed65a94421916e6c79ccf388889a60669d1e0e
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-15 09:33:34 -0600

    Add poro

--- app/poros/salary.rb
+++ app/poros/salary.rb
@@ -4,10 +4,42 @@ class Salary
               :forecast,
               :salaries
 
-  def initialize(salaries, forecast, destination)
-    @id = nil
-    @destination = destination.titleize
-    @forecast = get_forecast(forecast)
-    @salaries = get_salaries(salaries)
+  def initialize(data)
+    @destination = data[:destination]
+    @forecast    = data[:forecast]
+    @salaries    = data[:salaries]
   end
 end
+
+#
+# {
+#   "data": {
+#     "id": "null",
+#     "type": "salaries",
+#     "attributes": {
+
+
+#       "destination": "chicago",
+
+
+#       "forecast": {
+#         "summary": "Cloudy with a chance of meatballs",
+#         "temperature": "83 F"
+#       },
+
+
+#       "salaries": [
+#         {
+#           "title": "Data Analyst",
+#           "min": "$46,898.19",
+#           "max": "$67,929.19",
+#         },
+#           "title": "Data Scientist",
+#           "min": "$71,025.60",
+#           "max": "$103,647.55",
+#         },
+#         { ... etc ... }
+#       ]
+#     }
+#   }
+# }
