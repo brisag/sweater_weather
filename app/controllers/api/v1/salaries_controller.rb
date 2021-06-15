3790c2ad706440c1c729d6393e05dd8e34c64e5a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-15 10:12:12 -0600

    Refactored controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,48 +1,9 @@
-
 class Api::V1::SalariesController < ApplicationController
-
   def index
+    # binding.pry
     forecast = WeatherFacade.get_forecast(params[:destination])
-    salary_data = TeleportService.find_salaries(params[:destination])
+    salary_data = TeleportFacade.get_salaries(params[:destination], forecast)
 
-
-    job_titles = ['Data Analyst',
-                    'Data Scientist',
-                    'Mobile Developer',
-                    'QA Engineer',
-                    'Software Engineer',
-                    'Systems Administrator',
-                    'Web Developer']
-
-    jobs = salary_data[:salaries].find_all do |job_data|
-      job_titles.include?(job_data[:job][:title])
-    end
-
-    salary_info = {
-      destination: params[:destination],
-      forecast: salary_forecast(forecast.current_weather),
-      salaries: salary_info(jobs)
-    }
-
-    render json: SalariesSerializer.new(Salary.new(salary_info))
-  end
-
-  private
-
-  def salary_forecast(current_weather)
-    {
-      summary: current_weather.conditions,
-      temperature: current_weather.temperature
-    }
-  end
-
-  def salary_info(jobs)
-    jobs.map do |data|
-      {
-        title: data[:job][:title],
-        min: data[:salary_percentiles][:percentile_25],
-        max: data[:salary_percentiles][:percentile_75]
-      }
-    end
+    render json: SalariesSerializer.new(salary_data)
   end
 end
