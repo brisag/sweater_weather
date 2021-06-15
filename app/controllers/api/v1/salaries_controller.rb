5c51f6e6f3da222a4ec4d2a86c743d87f108f935
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-15 10:07:18 -0600

    Add all logic to controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,5 +1,48 @@
-class Api::V1::SalariesController < ApplicationController
-  def index
 
+class Api::V1::SalariesController < ApplicationController
+
+  def index
+    forecast = WeatherFacade.get_forecast(params[:destination])
+    salary_data = TeleportService.find_salaries(params[:destination])
+
+
+    job_titles = ['Data Analyst',
+                    'Data Scientist',
+                    'Mobile Developer',
+                    'QA Engineer',
+                    'Software Engineer',
+                    'Systems Administrator',
+                    'Web Developer']
+
+    jobs = salary_data[:salaries].find_all do |job_data|
+      job_titles.include?(job_data[:job][:title])
+    end
+
+    salary_info = {
+      destination: params[:destination],
+      forecast: salary_forecast(forecast.current_weather),
+      salaries: salary_info(jobs)
+    }
+
+    render json: SalariesSerializer.new(Salary.new(salary_info))
+  end
+
+  private
+
+  def salary_forecast(current_weather)
+    {
+      summary: current_weather.conditions,
+      temperature: current_weather.temperature
+    }
+  end
+
+  def salary_info(jobs)
+    jobs.map do |data|
+      {
+        title: data[:job][:title],
+        min: data[:salary_percentiles][:percentile_25],
+        max: data[:salary_percentiles][:percentile_75]
+      }
+    end
   end
 end
