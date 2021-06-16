32875fef1cef9121b1794ab660281a204756da7b
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-15 21:39:06 -0600

    Start trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,11 @@
 class RoadTripFacade
   class << self
-    # LocationService.newrequest_get_directions(orgin to destination)
-    # Weather of location when I arrive
-    # PORO = start_city, end_City, travle_time, weather
+    def trip_details(params)
+      origin = params[:origin]
+      destination = params[:destination]
+
+      directions = LocationService.get_directions(origin, destination)
+      # return trip info
+    end
   end
 end
