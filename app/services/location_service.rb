6389d061e3497b5ef7f066d19201034d493c25bc
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-16 07:14:31 -0600

    Add duration method

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -5,6 +5,15 @@ class LocationService
         req.params['key'] = ENV['MAPQ_API_KEY']
         req.params['location'] = location
       end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def get_duration(start, destination)
+      response = conn.get('directions/v2/route') do |req|
+        req.params['key'] = ENV['MAPQ_API_KEY']
+        req.params['from'] = start
+        req.params['to'] = destination
+      end
       parse_data(response)
     end
 
