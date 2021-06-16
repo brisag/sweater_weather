5d3e840ef78a735b8a6cb5a6713216a17b94769c
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-16 01:53:52 -0600

    Add sad path to backgrounds

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::BackgroundsController < ApplicationController
+  before_action :validate_params
+
   def index
     image = BackgroundFacade.image(params[:location])
     render json: ImageSerializer.new(image)
+  end
+
+  private
+
+  def validate_params
+    if params[:location].blank?
+      render json: { error: "Must provide location" }, status: :bad_request
+    end
   end
 end
